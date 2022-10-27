9aec3ae332a6d05047c3c39fbb364a7ac6397299
Mir Muhammad Abdur Rahman <mma4gp@virginia.edu>
2022-10-27 15:37:50 -0400

    HW09

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='booklover',
+      version='0.1',
+      description='BookLover class and BookLover test suite',
+      url='https://github.com/Zyqoz/booklover_package',
+      author='Zyqoz',
+      author_email='MirMuhammadA@gmail.com',
+      license='None',
+      packages=[])
